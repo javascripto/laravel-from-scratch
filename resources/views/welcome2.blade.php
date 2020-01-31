ea9fcdee426b0db917d369f4bc4d8e981037868d
Yuri Almeida <yurialves2@gmail.com>
2020-01-31 00:11:57 +0000

    passing data to view

--- resources/views/welcome2.blade.php
+++ resources/views/welcome2.blade.php
@@ -8,5 +8,25 @@
 </head>
 <body>
     <a href="/about">About</a>
+    <h1><?= $title; ?></h1>
+    <h2><?= $subtitle; ?></h2>
+    <a href="<?= $link ?>"><?= $video; ?></a>
+
+
+    <h3>Watched Videos:</h3>
+    <ul>
+    @foreach ($watched as $w_video)
+        <li>
+        @if ($w_video == $video)
+            <a href="{{ $link }}">{{ $video }}</a>
+        @else
+            {{ $w_video }}
+        @endif
+        </li>
+            
+    @endforeach
+    </ul>
+
+
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,23 @@
 */
 
 Route::get('/', function () {
-    return view('welcome2');
+    $video = 'Pass Data to Your Views';
+    $link = 'https://laracasts.com/series/laravel-from-scratch-2017/episodes/5';
+
+    $watched = [
+        'Laravel Installation and Composer',
+        'Basic Routing and Views',
+        'Laravel Valet is Your Best Friend',
+        'Database Setup and Sequel Pro',
+        'Pass Data to Your Views'
+    ];
+
+    return view('welcome2', [
+        'title' => 'Laracast'
+    ])
+    ->with('subtitle', 'Laravel 5.4 from scratch')
+    ->with(compact('video', 'link', 'watched'))
+    ;
 });
 
 Route::get('/about', function () {
